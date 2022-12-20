398eccc979b9f8e6cd5f0eff4e66131d25f261dc
chaeyeonhan1225 <gkscodus11@gmail.com>
2022-12-20 17:19:09 +0900

    init

---
+++ src/main/kotlin/com/interpark/tripleworkapi/TripleWorkApiApplication.kt
@@ -0,0 +1,11 @@
+package com.interpark.tripleworkapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TripleWorkApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TripleWorkApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/interpark/tripleworkapi/TripleWorkApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.interpark.tripleworkapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TripleWorkApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
